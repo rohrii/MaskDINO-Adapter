794a7f612b597aee350f765768d2a7353700b39f
david-rohrschneider <david.rohrschneider@gmail.com>
2024-09-27 17:46:52 +0000

    add shell scripts

---
+++ train_lora.sh
@@ -0,0 +1,29 @@
+    # --config-file configs/coco/instance-segmentation/swin/maskdino_R50_bs16_50ep_4s_dowsample1_2048.yaml \
+    # MODEL.WEIGHTS maskdino_swinl_50ep_300q_hid2048_3sd1_instance_maskenhanced_mask52.3ap_box59.0ap.pth \
+    # --resume \
+CUDA_VISIBLE_DEVICES=0,1 python train_net.py \
+    --num-gpus 2 \
+    --config-file configs/coco/instance-segmentation/maskdino_R50_bs16_50ep_3s_dowsample1_2048.yaml \
+    MODEL.WEIGHTS maskdino_r50_50ep_300q_hid2048_3sd1_instance_maskenhanced_mask46.3ap_box51.7ap.pth \
+    MODEL.SEM_SEG_HEAD.NUM_CLASSES <<NUM CLASSES>> \
+    MODEL.PIXEL_MEAN "[<<MEAN_CHANNEL1>>, <<MEAN_CHANNEL2>>, <<MEAN_CHANNEL3>>]" \
+    MODEL.PIXEL_STD "[<<STD_CHANNEL1>>, <<STD_CHANNEL2>>, <<STD_CHANNEL3>>]" \
+    SOLVER.IMS_PER_BATCH 4 \
+    SOLVER.BASE_LR 0.001 \
+    SOLVER.IGNORE_FIX "['mask_embed', 'class_embed', 'bbox_embed']" \
+    SOLVER.MAX_ITER <<NUM OPTIMIZER STEPS>> \
+    SOLVER.CHECKPOINT_PERIOD <<NUM STEPS FOR CHECKPOINT>> \
+    SOLVER.STEPS "(<<LR_STEP1>>, <<LR_STEP2>>, ...)" \
+    TEST.EVAL_PERIOD <<NUM STEPS FOR EVAL>> \
+    DATASETS.TRAIN "(<<DATASET_NAME_TRAIN>>,)" \
+    DATASETS.TEST "(<<DATASET_NAME_VAL>>,)" \
+    INPUT.IMAGE_SIZE <<INPUT IMAGE SIZE>> \
+    INPUT.MIN_SCALE 0.9 \
+    INPUT.MAX_SCALE 2.0 \
+    INPUT.DATASET_MAPPER_NAME <<DATASET_MAPPER NAME>> \
+    OUTPUT_DIR <<OUTPUT FILE DIR>> \
+    USE_LORA True \
+    LORA_DEFORMABLE_TARGETS "['sampling_offsets', 'value_proj']" \
+    LORA_TARGETS "['q', 'v']" \
+    LORA_RANK 8 \
+    LORA_ALPHA 8
